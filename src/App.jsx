8bfca8d820ac518982b0b729204b9f1b27d41020
Romesh Lankathilaka <romeshl@missionreadyhq.com>
2024-06-12 20:43:03 +1200

    Added the fetch method

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,40 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState({});
+  
+  useEffect(() => {
+    fetchData();
+  },[])
+  
+  async function fetchData() {
+    try {
+      const response = await ((await fetch('https://world-api-backend.azurewebsites.net/')).json());
+      setData(response);
+    }
+    catch (error) {
+      console.log("Error occured :" + error);
+      setData({});
+    }
+  }
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='my-[100px] mx-auto w-[500px] p-[20px] border font-mono'>
+      <h1 className="text-red-500 text-5xl underline">Countries</h1>
+      { Object.entries(data).map((item, index) => {
+        return (
+          <div key={index} className="flex gap-3">
+            <h1>{index + 1}</h1>
+            <p>{item[1]['Name']}</p>
+           </div>
+        )
+      })}
+     </div>
+  
   )
 }
 
