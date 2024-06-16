5cda3f264947424d997f9c87b4b63e645bcefb64
Romesh Lankathilaka <romeshl@missionreadyhq.com>
2024-06-16 20:16:52 +1200

    Updated the Toolbar title and favicon in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+
+import { useEffect } from 'react';
 import Countries from "./pages/Countries";
 import './App.css'
 
+
+import icon from "./assets/world.png";
+
 function App() {
+    
+  useEffect(() => {
+    let link = document.querySelector("link[rel~='icon']");
+    if (!link) {
+      link = document.createElement('link');
+      link.rel = 'icon';
+      document.getElementsByTagName('head')[0].appendChild(link);
+    }
+    link.href = icon;
+    document.title = "World API front-end";
+  }, []);
 
   return (
     <>
