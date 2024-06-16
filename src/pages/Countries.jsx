fcb953245f454f8535b915ce1f515f1fb92e0c38
Romesh Lankathilaka <romeshl@missionreadyhq.com>
2024-06-16 20:03:41 +1200

    replace the useEffect function with swr package for fetching data in Countries.jsx

--- src/pages/Countries.jsx
+++ src/pages/Countries.jsx
@@ -1,35 +1,51 @@
-import { useState, useEffect } from 'react';
-
+import useSWR from 'swr';
 
 export default function Countries() {
-    const [data, setData] = useState([]);
 
-    useEffect(() => {
-        fetchData();
-    }, [])
+    //const fetcher = url => fetch(url).then(r => r.json());
 
-    async function fetchData() {
-        try {
-            const response = await ((await fetch('https://world-api-backend.azurewebsites.net/')).json());
-            setData(response);
-        }
-        catch (error) {
-            console.log("Error occured :" + error);
-            setData([]);
-        }
+//    function fetcher(url){
+//        return fetch(url) 
+//            .then(response => response.json())
+//            .then(data);
+//     }
+    
+    async function fetcher(url) {
+        return await (await fetch(url)).json();
     }
+
+    // async function fetcher(url) {
+    //     const response = await fetch(url);
+    //     const data = await response.json();
+    //     return data;
+    // }
+
+    const { data, error, isLoading } = useSWR('https://world-api-backend.azurewebsites.net/', fetcher);
+    
     return (
         <>
             <div className='my-[100px] mx-auto w-[500px] p-[20px] border font-mono'>
-                <h1 className="text-red-500 text-5xl underline">Countries</h1>
-                {data.map((item, index) => {
-                    return (
-                        <div key={index} className="flex gap-3">
-                            <h1>{index + 1}</h1>
-                            <p>{item.Name}</p>
-                        </div>
-                    )
-                })}
+                {isLoading ?
+                    <h1>Loading countries data ...</h1>
+                    :
+                    error ?
+                        <h1>Error occurred while loading data. Error code:</h1>
+                        :
+                        data ?
+                            <div>
+                                <h1 className="text-red-500 text-5xl underline">Countries</h1>
+                                {data.map((item, index) => {
+                                    return (
+                                        <div key={index} className="flex gap-3">
+                                            <h1>{index + 1}</h1>
+                                            <p>{item.Name}</p>
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                            :
+                            <h1>No data to display.</h1>
+                }
             </div>
         </>
     )
